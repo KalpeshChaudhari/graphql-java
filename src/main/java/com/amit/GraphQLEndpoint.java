aad9abf72f5e01823c665379e6f66bd7945fd114
Amit Mujawar <amit.mujawar@mindstix.com>
2018-03-12 18:28:10 +0530

    Added mongo support

--- src/main/java/com/amit/GraphQLEndpoint.java
+++ src/main/java/com/amit/GraphQLEndpoint.java
@@ -3,6 +3,8 @@ package com.amit;
 import javax.servlet.annotation.WebServlet;
 
 import com.coxautodev.graphql.tools.SchemaParser;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import graphql.schema.GraphQLSchema;
 import graphql.servlet.SimpleGraphQLServlet;
 
@@ -14,15 +16,24 @@ import graphql.servlet.SimpleGraphQLServlet;
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
 
-    public GraphQLEndpoint() {
-        super(buildSchema(new LinkRepository()));
+    private static final LinkRepository linkRepository;
+
+    static {
+        //Change to `new MongoClient("mongodb://<host>:<port>/hackernews")`
+        //if you don't have Mongo running locally on port 27017
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
     }
 
-    private static GraphQLSchema buildSchema(LinkRepository linkRepository) {
-      return SchemaParser.newParser()
-          .file("schema.graphqls")
-          .resolvers(new Query(linkRepository), new Mutation(linkRepository))
-          .build()
-          .makeExecutableSchema();
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository), new Mutation(linkRepository))
+                .build()
+                .makeExecutableSchema();
     }
 }
