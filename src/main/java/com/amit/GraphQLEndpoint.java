fd4cb1d64a9f60ecc40105c5f3fc101e3c7877ef
Amit Mujawar <amit.mujawar@mindstix.com>
2018-03-12 17:20:56 +0530

    initial checkin

---
+++ src/main/java/com/amit/GraphQLEndpoint.java
@@ -0,0 +1,28 @@
+package com.amit;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+/**
+ * GraphQL Endpoint
+ *
+ * @author amit
+ */
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(buildSchema(new LinkRepository()));
+    }
+
+    private static GraphQLSchema buildSchema(LinkRepository linkRepository) {
+      return SchemaParser.newParser()
+          .file("schema.graphqls")
+          .resolvers(new Query(linkRepository), new Mutation(linkRepository))
+          .build()
+          .makeExecutableSchema();
+    }
+}
